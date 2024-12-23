33d80df757da504c13a1def47258695833cb162e
AS-Aurora <aayushsharma.lucky97@gmail.com>
2024-12-23 18:38:20 +0530

    Learned clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <header className="flex justify-between">
+            <h1>Clerk</h1>
+            <UserButton showName />
+          </header>
+          <main>
+
+          <SignedOut>
+            <SignIn routing="hash" />
+          </SignedOut>
+          <SignedIn>
+
+          {children}
+          </SignedIn>
+          </main>
+            
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
